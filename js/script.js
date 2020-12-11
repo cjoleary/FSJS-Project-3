a4c678b51523bc3d099cccf92fba12717cf118e1
cjoleary <cjoleary17@gmail.com>
2020-12-11 13:15:59 -0700

    updates t-shirt color options based on which t-shirt design the user choose

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ jobRole.addEventListener( 'change', (e) => {
     if ( e.target.value === 'other' ) {
         otherJobRole.style.display = 'block';
     }
-})
+});
 
 // ==================== //
 // T-SHIRT INFO SECTION //
@@ -29,3 +29,22 @@ const colorOptions = tshirtColor.children;
 
 // disables shirt color select element by default until user selects a shirt designxsx
 tshirtColor.disabled = true;
+
+// t-shirt design select element event listener
+tshirtDesign.addEventListener( 'change', (e) => {
+    // enables color select element when user selects a t-shirt design
+    tshirtColor.disabled = false;
+
+    for ( let i = 0; i < colorOptions.length; i++ ) {
+        const colorDataTheme = colorOptions[i].getAttribute('data-theme');
+
+        // updates color options based on which t-shirt design the user chooses. colors will only be selectable if they are specific to the design chosen
+        if ( e.target.value === colorDataTheme ) {
+            colorOptions[i].hidden = false;
+            colorOptions[i].selected = true;
+        } else {
+            colorOptions[i].hidden = true;
+            colorOptions[i].selected = false;
+        }
+    }
+});
